f71039fdafba6f61f0f93951ed4c4b5b46ebcfb3
Abhishek Shukla <90345678+bigbull699@users.noreply.github.com>
2021-11-20 12:52:04 +0530

    Update android.sh

--- android.sh
+++ android.sh
@@ -2,9 +2,9 @@ pkg update
 pkg upgrade -y
 pkg install php-apache git -y
 rm /data/data/com.termux/files/usr/etc/apache2/httpd.conf
-wget https://raw.githubusercontent.com/soal001/jiotv/master/httpd.conf
+wget https://raw.githubusercontent.com/bigbull699/jiotv/master/httpd.conf
 mv httpd.conf /data/data/com.termux/files/usr/etc/apache2/
-git clone https://github.com/soal001/jiotv
+git clone https://github.com/bigbull699/jiotv
 cp -r jiotv/* /data/data/com.termux/files/usr/share/apache2/default-site/htdocs && rm -rf jiotv/
 rm /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/http.conf /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/android.sh /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/jiotv.sh
 ip route get 1.2.3.4 | awk '{print $7}' > ipaddr.txt
